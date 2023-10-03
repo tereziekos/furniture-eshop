86a559cbac73a8b3f54dc783869f183601c4e704
Terezie Kosíková <89423234+tereziekos@users.noreply.github.com>
2023-10-03 11:54:30 +0200

    feat: add basic categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Category {
+  title: string;
+  subtitle: string;
+  imageUrl: string;
+  id: number;
+  linkUrl: string;
 }
 
+const App = () => {
+  const categories: Category[] = [
+    {
+      title: "Sofas",
+      subtitle: "Shop now",
+      imageUrl:
+        "https://www.ikea.com/images/ektorp-three-seat-sofa-10-year-guarantee__1364485295954-s5-250x250.jpg",
+      id: 1,
+      linkUrl: "sofas",
+    },
+    {
+      title: "Armchairs",
+      subtitle: "Shop now",
+      imageUrl:
+        "https://www.ikea.com/images/ektorp-three-seat-sofa-10-year-guarantee__1364485295954-s5-250x250.jpg",
+      id: 2,
+      linkUrl: "armchairs",
+    },
+
+    {
+      title: "Dining Chairs",
+      subtitle: "Shop now",
+      imageUrl:
+        "https://www.ikea.com/images/ektorp-three-seat-sofa-10-year-guarantee__1364485295954-s5-250x250.jpg",
+      id: 3,
+      linkUrl: "dining-chairs",
+    },
+    {
+      title: "Stools",
+      subtitle: "Shop now",
+      imageUrl:
+        "https://www.ikea.com/images/ektorp-three-seat-sofa-10-year-guarantee__1364485295954-s5-250x250.jpg",
+      id: 4,
+      linkUrl: "stools",
+    },
+    {
+      title: "Dining room",
+      subtitle: "Shop now",
+      imageUrl:
+        "https://www.ikea.com/images/ektorp-three-seat-sofa-10-year-guarantee__1364485295954-s5-250x250.jpg",
+      id: 5,
+      linkUrl: "dining-room",
+    },
+    {
+      title: "Living room",
+      subtitle: "Shop now",
+      imageUrl:
+        "https://www.ikea.com/images/ektorp-three-seat-sofa-10-year-guarantee__1364485295954-s5-250x250.jpg",
+      id: 6,
+      linkUrl: "living-room",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title, subtitle, imageUrl, id }) => (
+        <div className="category-container" key={id}>
+          <img src={imageUrl} alt={title} />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>{subtitle}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default App;
